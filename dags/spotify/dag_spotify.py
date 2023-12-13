4f8c1db95309b5947a8c488e2e1816ffa34a7c68
nacho dorado <natxo98@gmail.com>
2023-12-13 18:46:24 +0100

    Obtencion del token

--- dags/spotify/dag_spotify.py
+++ dags/spotify/dag_spotify.py
@@ -2,12 +2,28 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 import requests
+import json
+
+from spotify.config import CLIENTE_ID, CLIENTE_SECRETO
+
+# Funcion para obtener el token
+def generarToken(cliente_id:str, cliente_secreto:str)->str:
+
+	url_token="https://accounts.spotify.com/api/token"
+
+	data={"grant_type": "client_credentials", "client_id": cliente_id, "client_secret":cliente_secreto}
+
+	respuesta=requests.post(url_token, data=data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+
+	contenido=json.loads(respuesta.content)
+
+	return contenido["access_token"]
 
 
 # Funcion para extraer los datos de la API de Spotify
 def extraccion()->None:
 
-	token=""
+	token=generarToken(CLIENTE_ID, CLIENTE_SECRETO)
 
 	id_artista="2R21vXR83lH98kGeO99Y66"
 
@@ -26,7 +42,7 @@ def extraccion()->None:
 
 
 with DAG("spotify_dag", start_date=datetime(2023,12,12), description="DAG para obtener datos de la API de Spotify",
-		schedule_interval=timedelta(minutes=1), catchup=False) as dag:
+		schedule_interval=timedelta(minutes=60), catchup=False) as dag:
 
 	extraccion_data=PythonOperator(task_id="extracion_data", python_callable=extraccion)
 
