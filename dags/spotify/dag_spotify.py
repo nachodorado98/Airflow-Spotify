ce54ff6160f1023d1b125bc40750687d61377df5
nacho dorado <natxo98@gmail.com>
2023-12-15 13:05:56 +0100

    Creacion de la conexion con Postgres. Tarea de carga

--- dags/spotify/dag_spotify.py
+++ dags/spotify/dag_spotify.py
@@ -3,9 +3,21 @@ from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 from airflow.exceptions import AirflowSkipException
 import requests
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from spotify.autenticacion.auth import generarToken
 from spotify.autenticacion.config import CLIENTE_ID, CLIENTE_SECRETO
+
+from spotify.database.conexion import crearHook
+
+# Funcion para crear la tabla de las canciones
+def crearTabla(hook:PostgresHook=crearHook())->None:
+
+	hook.run("""CREATE TABLE IF NOT EXISTS canciones (id VARCHAR(30) PRIMARY KEY,
+														nombre VARCHAR(100),
+														artistas VARCHAR(100),
+														album VARCHAR(100),
+														fecha DATE);""")
 
 # Funcion para extraer los datos de la API de Spotify
 def extraccion(**kwarg)->None:
@@ -61,17 +73,23 @@ def transformacion(**kwarg)->None:
 	print("Transformacion correcta")
 
 # Funcion para cargar los datos de la API de Spotify
-def carga(**kwarg)->None:
+def carga(hook:PostgresHook=crearHook(), **kwarg)->None:
 
-	canciones=kwarg["ti"].xcom_pull(key="data_transformada", task_ids="transformacion_data")
+	data=kwarg["ti"].xcom_pull(key="data_transformada", task_ids="transformacion_data")
+
+	canciones=[tuple(cancion) for cancion in data]
 
 	for cancion in canciones:
 
-		print(cancion)
+		print(f"Insertando la cancion '{cancion[1]}'...")
 
+		hook.run("INSERT INTO canciones VALUES %s", parameters=(cancion,))
+		
 
 with DAG("spotify_dag", start_date=datetime(2023,12,12), description="DAG para obtener datos de la API de Spotify",
-		schedule_interval=timedelta(minutes=60), catchup=False) as dag:
+		schedule_interval=timedelta(days=1), catchup=False) as dag:
+
+	creacion_tabla=PythonOperator(task_id="creacion_tabla", python_callable=crearTabla)
 
 	extraccion_data=PythonOperator(task_id="extracion_data", python_callable=extraccion)
 
@@ -79,4 +97,4 @@ with DAG("spotify_dag", start_date=datetime(2023,12,12), description="DAG para o
 
 	carga_data=PythonOperator(task_id="carga_data", python_callable=carga)
 
-	extraccion_data >> transformacion_data >> carga_data
+	creacion_tabla >> extraccion_data >> transformacion_data >> carga_data
