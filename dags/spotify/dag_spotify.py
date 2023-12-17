77856f5807676b537df075a61944877e91dbce4c
nacho dorado <natxo98@gmail.com>
2023-12-17 23:11:28 +0100

    Extraccion completa desde el inicio

--- dags/spotify/dag_spotify.py
+++ dags/spotify/dag_spotify.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.exceptions import AirflowSkipException
 import requests
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 
 from spotify.autenticacion.auth import generarToken
 from spotify.autenticacion.config import CLIENTE_ID, CLIENTE_SECRETO
@@ -21,7 +21,6 @@ def existe_tabla(hook:PostgresHook=crearHook())->str:
 
 	return "no_creacion_tabla" if (TABLA,) in tablas else "creacion_tabla"
 
-
 # Funcion que no crea la tabla de las canciones
 def noCrearTabla(hook:PostgresHook=crearHook())->None:
 
@@ -31,48 +30,65 @@ def noCrearTabla(hook:PostgresHook=crearHook())->None:
 
 	print(f"Numero de registros: {numero_registros}")
 
+	raise AirflowSkipException("Parada")
 
 # Funcion para crear la tabla de las canciones
 def crearTabla(hook:PostgresHook=crearHook())->None:
 
 	hook.run(f"""CREATE TABLE {TABLA} (id VARCHAR(30) PRIMARY KEY,
-										nombre VARCHAR(100),
-										artistas VARCHAR(100),
-										album VARCHAR(100),
+										nombre VARCHAR(200),
+										artistas VARCHAR(200),
+										album VARCHAR(200),
 										fecha DATE);""")
 	
 	print(f"Tabla {TABLA} creada correctamente")
 
+# Funcion para realizar una peticion a la API
+def peticion_API(token:str, salto:int=0, limite:int=100)->Optional[Dict]:
+
+	url=f"https://api.spotify.com/v1/playlists/{PLAYLIST}/tracks?limit={limite}&offset={salto}"
+
+	respuesta=requests.get(url, headers={"Authorization":f"Bearer {token}"})
+
+	if respuesta.status_code==200:
+
+		return respuesta.json()
+
+	else:
+		
+		print(respuesta.status_code)
+
+		raise AirflowSkipException("Error en la extraccion")
+
+# Funcion para la iteracion en la extraccion desde el salto hasta el final
+def extraerCanciones(salto:int=0)->List[Dict]:
+
+	token=generarToken(CLIENTE_ID, CLIENTE_SECRETO)
+
+	items=[]
+
+	while True:
+
+		contenido=peticion_API(token, salto)
+
+		if len(contenido["items"])==0:
+
+			break
+
+		items+=contenido["items"]
+
+		salto+=100
+
+	return items
 
 # Funcion para extraer los datos de la API de Spotify
 def extraccion(**kwarg)->None:
 
-	# Funcion para realizar una peticion a la API
-	def peticion_API()->Optional[Dict]:
+	canciones=extraerCanciones()	
 
-		token=generarToken(CLIENTE_ID, CLIENTE_SECRETO)
-
-		url=f"https://api.spotify.com/v1/playlists/{PLAYLIST}/tracks?limit=100&offset=0"
-
-		respuesta=requests.get(url, headers={"Authorization":f"Bearer {token}"})
-
-		if respuesta.status_code==200:
-
-			return respuesta.json()
-
-		else:
-		
-			print(respuesta.status_code)
-
-			raise AirflowSkipException("Error en la extraccion")
-
-
-	contenido=peticion_API()
-
-	kwarg["ti"].xcom_push(key="data_extraida", value=contenido)
+	kwarg["ti"].xcom_push(key="data_extraida", value=canciones)
 
 	print("Extraccion correcta")
-
 
 # Funcion para transformar los datos de la API de Spotify
 def transformacion(**kwarg)->None:
@@ -80,9 +96,9 @@ def transformacion(**kwarg)->None:
 	data=kwarg["ti"].xcom_pull(key="data_extraida", task_ids="extracion_data")
 
 	# Funcion para limpiar la cancion
-	def limpiarCancion(cancion:dict)->tuple:
+	def limpiarCancion(cancion:Dict)->tuple:
 
-		id=cancion["track"]["id"]
+		id_cancion=cancion["track"]["id"]
 
 		nombre=cancion["track"]["name"]
 
@@ -94,9 +110,9 @@ def transformacion(**kwarg)->None:
 
 		fecha=cancion["added_at"].split("T")[0]
 
-		return id, nombre, artistas_cadena, album, fecha
+		return id_cancion, nombre, artistas_cadena, album, fecha
 
-	canciones=list(map(limpiarCancion, data["items"]))
+	canciones=list(map(limpiarCancion, data))
 
 	kwarg["ti"].xcom_push(key="data_transformada", value=canciones)
 
@@ -113,11 +129,19 @@ def carga(hook:PostgresHook=crearHook(), **kwarg)->None:
 
 		print(f"Insertando la cancion '{cancion[1]}'...")
 
-		hook.run(f"INSERT INTO {TABLA} VALUES %s", parameters=(cancion,))
-		
+		try:
 
-with DAG("spotify_dag", start_date=datetime(2023,12,12), description="DAG para obtener datos de la API de Spotify",
-			schedule_interval=timedelta(days=1), catchup=False) as dag:
+			hook.run(f"INSERT INTO {TABLA} VALUES %s", parameters=(cancion,))
+
+		except:
+
+			print(f"Error en la insercion de la cancion '{cancion[1]}' con ID '{cancion[0]}'")
+		
+	print("Carga correcta")
+
+
+with DAG("spotify_dag", start_date=datetime(2023,12,16), description="DAG para obtener datos de la API de Spotify",
+			schedule_interval=timedelta(days=7), catchup=False) as dag:
 
 	comprobacion_tabla=BranchPythonOperator(task_id="existe_tabla", python_callable=existe_tabla)
 
